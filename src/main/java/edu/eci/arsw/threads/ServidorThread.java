3504a4d0655f42eea49817407a4b6b46a4807425
YesidCarrillo26 <yesid.carrillo@mail.escuelaing.edu.co>
2022-08-10 19:02:10 -0500

    server thread

--- src/main/java/edu/eci/arsw/threads/ServidorThread.java
+++ src/main/java/edu/eci/arsw/threads/ServidorThread.java
@@ -1,10 +1,19 @@
 package edu.eci.arsw.threads;
 
+import edu.eci.arsw.blacklistvalidator.HostBlackListsValidator;
+
 public class ServidorThread extends Thread {
-	public ServidorThread(){
-		
+	private HostBlackListsValidator hostBlackListsValidator;
+	private String ip;
+	private int n;
+
+
+	public ServidorThread(String ip, int n){
+		this.ip = ip;
+		this.n = n;
 	}
 	public void searchOcu() {
+		hostBlackListsValidator.checkHost(ip,n);
 		
 	}
 }
